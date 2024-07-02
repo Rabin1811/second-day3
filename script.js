3e9e45ede5a4382dae701170d7f7a8f64f7ee99a
Rabin1811 <rabindraaryal05@gmail.com>
2024-07-02 10:10:52 +0545

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+// function sum(num1,num2){
+//     s = num1 +num2;
+//     console.log(s);
+// }
+
+// sum(50,20);
+
+
+const array =["a", "b", "c", "d", 5, 6, 7, 8, 9, 10];
+for (i of array){
+    console.log(i);
+}
+
+for (i in array)
+    {
+        console.log(i);
+    }
+const nestedArray [
+    1,
+    2,
+    3,
+    ["a", "b", "c"],
+    [4, 5, 6],
+    "d",
+    "e",
+    "f",
+    
+]
+    for (i of nestedArray)
+        {
+            console.log("")
+        }
